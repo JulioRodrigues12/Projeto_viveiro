094aa5215531d633eba40f2cd8c263e8cf937e82
JulioRodrigues12 <rodriguesjulio635@gmail.com>
2025-09-29 22:40:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AbaPrincipal } from './components/Home/AbaPrincipal';
-import { SobreNos } from './components/Sobre/SobreNos';
+import { SobreNos } from './components/Sobre';
 import { NossoTrabalho } from './components/Trabalho/NossoTrabalho';
 import { AbaFrete } from './components/Cep/AbaFrete';
 import { PedidoProvider } from './components/Pedido/Pedido';
